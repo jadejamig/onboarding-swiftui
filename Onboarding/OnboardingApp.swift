eab6548e426e27811f7cd1c233ca5cc76556ab32
Jade Jamig <ajamig016@gmail.com>
2021-05-31 13:18:17 +0800

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Anselm Jade Jamig on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
